e9c57eb426f239a0c51418a93292ccc87426a84a
TriDiscord <67373913+TriDiscord@users.noreply.github.com>
2022-07-02 16:01:17 +0300

    add lockscreen and changes

---
+++ lockscreen/js/index.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.style.backgroundImage = `url("https://raw.githubusercontent.com/XiaomiSimulator/SimulatorAssets/main/images/wallpapers/${
+    Math.floor(Math.random() * 23) + 1
+  }.jpg")`;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundAttachment = "fixed";
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var day_of_the_week = date.getDay();
+  day_of_the_week =
+    day_of_the_week == 0
+      ? "Sunday"
+      : day_of_the_week == 1
+      ? "Monday"
+      : day_of_the_week == 2
+      ? "Tuesday"
+      : day_of_the_week == 3
+      ? "Wednesday"
+      : day_of_the_week == 4
+      ? "Thursday"
+      : day_of_the_week == 5
+      ? "Friday"
+      : "Saturday";
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (day < 10) {
+    day = "0" + day;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+  var time = hours + ":" + minutes;
+  document.getElementsByClassName("clock_text")[0].innerHTML = time;
+  document.getElementsByClassName("date_text")[0].innerHTML = `${day}/${month} ${day_of_the_week}`;
+  setInterval(function () {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var day_of_the_week = date.getDay();
+    day_of_the_week =
+      day_of_the_week == 0
+        ? "Sunday"
+        : day_of_the_week == 1
+        ? "Monday"
+        : day_of_the_week == 2
+        ? "Tuesday"
+        : day_of_the_week == 3
+        ? "Wednesday"
+        : day_of_the_week == 4
+        ? "Thursday"
+        : day_of_the_week == 5
+        ? "Friday"
+        : "Saturday";
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+    var time = hours + ":" + minutes;
+    document.getElementsByClassName("clock_text")[0].innerHTML = time;
+    document.getElementsByClassName("date_text")[0].innerHTML = `${day}/${month} ${day_of_the_week}`;
+  }, 1000);
+});
